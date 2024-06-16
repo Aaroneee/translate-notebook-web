4848c3e03d802b9d47f37875a2a716ae0ab94237
Aaron <827052236@qq.com>
2024-06-16 12:39:07 +0800

    feat: 列表添加搜索框

--- src/pages/note-book-list.vue
+++ src/pages/note-book-list.vue
@@ -1,11 +1,24 @@
-<script setup>
 
-</script>
 
 <template>
-
+  <van-search
+      v-model="searchValue"
+      show-action
+      placeholder="请输入搜索关键词"
+  >
+    <template #action>
+      <div @click="searchBtn">搜索</div>
+    </template>
+  </van-search>
 </template>
+<script setup>
+import {ref} from "vue";
 
+const searchValue=ref("");
+function searchBtn(){
+  console.log(searchValue.value);
+}
+</script>
 <style scoped>
 
 </style>
